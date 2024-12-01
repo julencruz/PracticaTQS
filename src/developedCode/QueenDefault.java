409a28d7c5597b75b295c7f8d8e06ced9173163c
Julen Cruz <julencruzg@gmail.com>
2024-12-01 16:31:25 +0100

    corrected tests and developed code

--- src/developedCode/QueenDefault.java
+++ src/developedCode/QueenDefault.java
@@ -10,42 +10,42 @@ public class QueenDefault extends Queen{
 	public void disableSquares(int size) {
 		for(int i = 0; i < size; i++)
 		{
-			game.disableSquare(i, posY);
 			game.disableSquare(posX, i);
+			game.disableSquare(i, posY);
 		}
 		
-		if (posX-1 >= 0 && posY-1 >= 0) {
-			game.disableSquare(posX-1, posY-1);
-		}
 		if (posX-1 >= 0 && posY+1 < size) {
 			game.disableSquare(posX-1, posY+1);
+		}
+		if (posX+1 < size && posY+1 < size) {
+			game.disableSquare(posX+1, posY+1);
 		}
 		if (posX+1 < size && posY-1 >= 0) {
 			game.disableSquare(posX+1, posY-1);
 		}
-		if (posX+1 < size && posY+1 < size) {
-			game.disableSquare(posX+1, posY+1);
+		if (posX-1 >= 0 && posY-1 >= 0) {
+			game.disableSquare(posX-1, posY-1);
 		}
 	}
 
 	@Override
 	public void enableSquares(int size) {
 		for (int i = 0; i < size; i++) {
-			game.enableSquare(i, posY);
 			game.enableSquare(posX, i);
+			game.enableSquare(i, posY);
 		}
 		
-		if (posX-1 >= 0 && posY-1 >= 0) {
-			game.enableSquare(posX-1, posY-1);
-		}
 		if (posX-1 >= 0 && posY+1 < size) {
 			game.enableSquare(posX-1, posY+1);
+		}
+		if (posX+1 < size && posY+1 < size) {
+			game.enableSquare(posX+1, posY+1);
 		}
 		if (posX+1 < size && posY-1 >= 0) {
 			game.enableSquare(posX+1, posY-1);
 		}
-		if (posX+1 < size && posY+1 < size) {
-			game.enableSquare(posX+1, posY+1);
+		if (posX-1 >= 0 && posY-1 >= 0) {
+			game.enableSquare(posX-1, posY-1);
 		}
 	}
 }
